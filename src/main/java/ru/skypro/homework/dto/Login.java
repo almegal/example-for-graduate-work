0bb5659d84cd435c9f753c525efd8840d09b0d43
aleksei <alekseipeikhel@gmail.com>
2024-09-03 09:18:58 +0300

    валидация дто Login, Register и добавления описаний api

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -1,10 +1,19 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.Size;
 import lombok.Data;
+import ru.skypro.homework.util.OpenApiConstant;
 
 @Data
+@ApiModel(value = "Login", description = "Модель пользователя")
 public class Login {
 
-    private String username;
+    @ApiModelProperty(value = "Пароль", example = OpenApiConstant.USER_PASS)
+    @Size(min = 4, max = 32)
     private String password;
+    @ApiModelProperty(value = "Имя пользователя", example = OpenApiConstant.USER_NAME)
+    @Size(min = 8, max = 16)
+    private String username;
 }
