b86b3ed13e3a021b33e9d83db351c82f81ce9777
Aleksey-0052 <136725827+Aleksey-0052@users.noreply.github.com>
2024-09-08 20:39:13 +0300

    Update AdDto.java
    Удалена пустая строка

--- src/main/java/ru/skypro/homework/dto/AdDto.java
+++ src/main/java/ru/skypro/homework/dto/AdDto.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
 @Data
 @ApiModel(value = "Ad", description = "Модель объявления")
 public class AdDto {
