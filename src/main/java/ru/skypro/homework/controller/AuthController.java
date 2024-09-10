ce948d8d704fb289d56a1f82bd6c82248c332039
aleksei <alekseipeikhel@gmail.com>
2024-09-10 05:05:20 +0300

    изменил параметры на соответствующие дто

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.Login;
+import ru.skypro.homework.dto.LoginDto;
 import ru.skypro.homework.service.AuthService;
 
 @Slf4j
@@ -27,7 +27,7 @@ public class AuthController {
 
     @ApiOperation(value = "Авторизация по логину и паролю",
             notes = "Позволяет пользователю войти в систему",
-            response = Login.class)
+            response = LoginDto.class)
     @ApiResponses(value = {
             @ApiResponse(
                     code = 200,
@@ -37,8 +37,9 @@ public class AuthController {
                     message = "Unauthorized")
     })
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody Login login) {
-        if (authService.login(login.getUsername(), login.getPassword())) {
+    public ResponseEntity<Void> login(@RequestBody LoginDto loginDto) {
+        ;
+        if (authService.login(loginDto.getUsername(), loginDto.getPassword())) {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
