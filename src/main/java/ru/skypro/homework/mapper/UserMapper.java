634192980f5ae335488f3e72aa4d32db6d6237a9
aleksei <alekseipeikhel@gmail.com>
2024-09-11 22:35:17 +0300

    добавлены мапперы

---
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package ru.skypro.homework.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import ru.skypro.homework.dto.NewPasswordDto;
+import ru.skypro.homework.dto.RegisterDto;
+import ru.skypro.homework.dto.UpdateUserDto;
+import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto toDto(User user);
+
+    User toEntity(UserDto dto);
+
+    void updateUserFromUpdateUserDto(UpdateUserDto dto,
+                                     @MappingTarget User user);
+
+    @Mapping(source = "newPassword", target = "password")
+    void updateUserPasswordFromDto(NewPasswordDto dto,
+                                   @MappingTarget User user);
+
+    @Mapping(source = "username", target = "email")
+    User toEntityFromRegisterDto(RegisterDto dto);
+
+}
